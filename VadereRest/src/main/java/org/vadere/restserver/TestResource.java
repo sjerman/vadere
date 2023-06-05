ba5d509bdce04a504f277144a23125fe3ff2975d
Steve Jerman <steve@kloudspot.com>
2023-06-05 14:47:56 +0100

    Update build

---
+++ VadereRest/src/main/java/org/vadere/restserver/TestResource.java
@@ -0,0 +1,16 @@
+package org.vadere.restserver;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestResource {
+
+  @GetMapping(value = "/login-logo", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String ping() {
+    return "ok";
+  }
+}
